7fd5ba1924a8247a4210d7c6a964a05991f58901
doyoung930 <70666642+doyoung930@users.noreply.github.com>
2023-09-12 11:59:25 +0900

    0911 수업

---
+++ 2023_09_11/name.py
@@ -0,0 +1,43 @@
+import turtle
+
+turtle.penup()
+turtle.goto(-100, 0)
+turtle.pendown()
+turtle.circle(50)
+
+
+
+turtle.penup()
+turtle.goto(- 30, -50)
+turtle.pendown()
+turtle.goto(- 30, 150)
+
+turtle.penup()
+turtle.goto( 0, -50)
+turtle.pendown()
+turtle.goto( 150, -50)
+turtle.goto( 75, -50)
+turtle.goto( 75, 0)
+turtle.goto( 130, 0)
+turtle.goto( 20, 0)
+turtle.goto( 20, 75)
+turtle.goto( 130, 75)
+
+turtle.penup()
+turtle.goto(245, -50)
+turtle.pendown()
+turtle.circle(30)
+turtle.penup()
+turtle.goto(225, 30)
+turtle.pendown()
+turtle.circle(30)
+turtle.penup()
+turtle.goto(255, 45)
+turtle.pendown()
+turtle.goto(270, 45)
+turtle.goto(270, 15)
+turtle.goto(270, 105)
+turtle.goto(270, 75)
+turtle.goto(255, 75)
+
+turtle.exitonclick()
